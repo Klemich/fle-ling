18c9c8e727f2f97e188928d6ec30fa629c8c4bf0
Klemich <c.michard@live.com>
2022-09-01 16:45:27 +0200

    Add files via upload

---
+++ siteparts/partials/footer.php
@@ -0,0 +1,15 @@
+<!-- FOOTER -->
+<footer class="container">
+  <p class="float-end"><a href="#">Back to top</a></p>
+  <p>&copy; 2017–2022 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+</footer>
+</main>
+
+
+  <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+
+
+</body>
+</html>
+
+<!--Footer section-->
